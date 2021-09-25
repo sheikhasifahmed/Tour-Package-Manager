8d4fe12f3c41486c2a741a3b859b5ce45a55682f
sheikasifahmed <sheikhasifahmed100@gmail.com>
2021-09-25 23:45:45 +0600

    testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div>
+      <h1>Welcome to the Complete Tour Package Manager</h1>
     </div>
   );
 }
